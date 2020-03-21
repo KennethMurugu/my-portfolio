0b718d018c359270b99c0af1ccdb4a665f699442
Kenneth Murugu <kennethmurugu@gmail.com>
2020-03-21 11:08:28 +0300

    changed main font, added some content to about

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="page about" ref="about-page">
-    <!-- <div class="header">
-      <h1>About Me</h1>
-      <i class="fas fa-times fa-lg close" @click="$emit('switchpage', 0)"></i>
-    </div>-->
     <PageHeader
       class="header"
       title="About"
@@ -13,6 +9,17 @@
       <p @click="$emit('switchpage', 2)">Projects</p>
       <p @click="$emit('switchpage', 3)">Say Hi</p>
     </PageHeader>
+
+    <div class="content">
+      <div class="quote">
+        <i class="fas fa-quote-left fa-lg quote-left"></i>
+        <h2 class="text">Passion gets you further.</h2>
+        <i class="fas fa-quote-right fa-lg quote-right"></i>
+        <p class="author">
+          <em>- Charles Karitu</em>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,5 +42,32 @@ export default {
   display: block;
   padding: 0;
   overflow: auto;
+
+  .content {
+    margin: 1.5rem;
+  }
+}
+
+.quote {
+  border: 2px solid #fff;
+  padding: 1rem 2rem;
+  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.4);
+  max-width: 500px;
+  margin: 2rem auto;
+  background-color: rgba(0, 0, 0, 0.267);
+  backdrop-filter: blur(10px);
+  display: flex;
+  flex-direction: column;
+
+  .text {
+    text-align: center;
+  }
+  .quote-right {
+    text-align: right;
+  }
+  .author {
+    text-align: center;
+    margin: 0;
+  }
 }
 </style>
--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="header" :style="{background}">
-    <i class="fas fa-bars fa-lg dropdown-toggle" @click="showDropdown = !showDropdown"></i>
-    <h1 class="title">{{title}}</h1>
-    <div class="right-container">
-      <div class="links">
-        <slot></slot>
-      </div>
-      <div class="close" @click="$emit('close')">
-        <i class="fas fa-times fa-lg"></i>
-      </div>
-    </div>
     <div class="dropdown-links" :class="{'hide': !showDropdown}">
       <slot></slot>
+    </div>
+    <div class="content">
+      <i class="fas fa-bars fa-lg dropdown-toggle" @click="showDropdown = !showDropdown"></i>
+      <h1 class="title">{{title}}</h1>
+      <div class="right-container">
+        <div class="links">
+          <slot></slot>
+        </div>
+        <div class="close" @click="$emit('close')">
+          <div class="bg"></div>
+          <i class="fas fa-times fa-lg"></i>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -28,11 +31,15 @@ export default class PageHeader extends Vue {
 <style lang="scss" scoped>
 .header {
   position: relative;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
   padding: 0 2rem;
+  .content {
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    z-index: 2;
+  }
 
   .dropdown-toggle {
     display: none;
@@ -49,8 +56,9 @@ export default class PageHeader extends Vue {
       & * {
         margin: 0 0.75rem;
         cursor: pointer;
-        border-bottom: 3px solid transparent;
+        border: 1px solid transparent;
         transition: all 0.2s ease;
+        padding: 5px 15px;
 
         &:hover {
           text-shadow: 0px 0px 5px rgba(255, 255, 255, 0.438);
@@ -60,25 +68,50 @@ export default class PageHeader extends Vue {
     }
 
     .close {
+      position: relative;
       cursor: pointer;
-      width: 30px;
-      height: 30px;
+      width: 40px;
+      height: 40px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      .bg {
+        background-color: rgba(255, 255, 255, 0.3);
+        border: 0px solid transparent;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        border-radius: 50%;
+        transition: all 0.3s ease;
+      }
+
+      &:hover .bg {
+        background-color: transparent;
+        border-width: 1px;
+        border-color: #fff;
+      }
     }
   }
 
   .dropdown-links {
-    background: rgba(0, 0, 0, 0.514);
+    background: rgba(0, 0, 0, 0.75);
     position: absolute;
     width: 100%;
     left: 0;
-    top: 53px;
+    top: 46px;
     opacity: 1;
     text-align: center;
     transition: all 0.35s ease-in-out;
+    z-index: 1;
+    transform-origin: top;
+    transform: scaleY(1);
 
     &.hide {
       opacity: 0;
       visibility: hidden;
+      transform: scaleY(0);
+      // top: 55px;
     }
 
     & * {
@@ -111,6 +144,11 @@ export default class PageHeader extends Vue {
     .right-container {
       .links {
         display: none;
+      }
+
+      .close .bg {
+        width: 90%;
+        height: 90%;
       }
     }
   }
