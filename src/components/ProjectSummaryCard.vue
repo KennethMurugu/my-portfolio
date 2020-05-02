1da5cb1ab725a22f0d5e7d4abf6adfde39adb865
Kenneth Murugu <kennethmurugu@gmail.com>
2020-05-02 10:19:25 +0300

    added ProjectSummaryCard [WIP]

---
+++ src/components/ProjectSummaryCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="project-summary">
+    <img
+      :src="require(`@/assets/img/projects/${project.thumbnail}`)"
+      :alt="project.thumbnail"
+      class="thumbnail"
+    />
+    <div class="text px-2 pb-2">
+      <p class="title mt-0">{{project.title}}</p>
+      <p class="description mt-0">{{project.description}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { project: {} },
+  name: 'ProjectSummaryCard'
+}
+</script>
+
+<style lang="scss" scoped>
+.project-summary {
+  border-radius: 5px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.445);
+  max-width: 350px;
+  overflow: hidden;
+  background-color: #192139;
+  transition: all 0.25s ease;
+
+  &:hover {
+    box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.61);
+  }
+}
+.thumbnail {
+  width: 100%;
+}
+
+.description {
+  font-size: 0.9em;
+}
+</style>
--- src/views/Pages/Projects.vue
+++ src/views/Pages/Projects.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="page projects">
+  <div class="page projects pb-5">
     <!-- <div class="header mx-auto mb-12">
       <div class="icons-container">
         <i class="fas fa-laptop-code fa-7x icon"></i>
@@ -9,7 +9,7 @@
       </div>
     </div>-->
 
-    <div class="speech-container mx-auto">
+    <div class="speech-container mx-auto mb-12">
       <img :src="require('@/assets/img/me.jpeg')" alt="Kenneth Kimotho" class="avatar-me mx-auto" />
       <div class="speech pa-5 pl-12">
         <p>
@@ -18,14 +18,44 @@
         </p>
       </div>
     </div>
+
+    <div class="project-list">
+      <ProjectSummaryCard v-for="(item, index) in projectList" :key="index" :project="item" />
+    </div>
   </div>
 </template>
 
 <script>
-// import PageHeader from '@/components/PageHeader'
+import ProjectSummaryCard from '@/components/ProjectSummaryCard'
 export default {
   name: 'Projects',
-  // components: { PageHeader },
+  components: { ProjectSummaryCard },
+  data() {
+    return {
+      projectList: [
+        {
+          thumbnail: 'project-1.png',
+          title: 'Some Title',
+          description: 'Some awesome description here'
+        },
+        {
+          thumbnail: 'project-1.png',
+          title: 'Some Title',
+          description: 'Some awesome description here'
+        },
+        {
+          thumbnail: 'project-1.png',
+          title: 'Some Title',
+          description: 'Some awesome description here'
+        },
+        {
+          thumbnail: 'project-1.png',
+          title: 'Some Title',
+          description: 'Some awesome description here'
+        }
+      ]
+    }
+  },
   methods: {
     close() {
       this.$emit('closeme')
@@ -96,6 +126,20 @@ export default {
     // backdrop-filter: blur(5px);
     z-index: 1;
     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.507);
+  }
+}
+
+.project-list {
+  display: grid;
+  grid-template-columns: auto auto;
+  align-items: center;
+  justify-content: center;
+  gap: 1.5rem 3rem;
+}
+
+@media screen and (max-width: 600px) {
+  .project-list {
+    grid-template-columns: auto;
   }
 }
 </style>
