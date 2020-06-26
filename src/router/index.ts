b634c31a34ebaeeb845ad519c82d03cdaea0c1c7
Kenneth Murugu(Ubuntu) <kennethmurugu@gmail.com>
2020-06-26 11:10:39 +0300

    cleanup

--- src/router/index.ts
+++ src/router/index.ts
@@ -27,50 +27,6 @@ const routes = [
     props: true,
     component: () => import('../views/Projects.vue')
   }
-  // {
-  //   path: '/pages',
-  //   redirect: '/pages/about',
-  //   props: true,
-  //   component: () => import('../views/Pages/PagesTemplate.vue'),
-  //   children: [
-  //     {
-  //       name: 'AboutPage',
-  //       path: 'about',
-  //       props: true,
-  //       meta: {
-  //         currentPage: {
-  //           name: 'about',
-  //           title: 'About Me'
-  //         }
-  //       },
-  //       component: () => import('../views/Pages/About.vue')
-  //     },
-  //     {
-  //       name: 'ProjectsPage',
-  //       path: 'projects',
-  //       props: true,
-  //       meta: {
-  //         currentPage: {
-  //           name: 'projects',
-  //           title: 'Projects'
-  //         }
-  //       },
-  //       component: () => import('../views/Pages/Projects.vue')
-  //     },
-  //     {
-  //       name: 'SayHiPage',
-  //       path: 'sayhi',
-  //       props: true,
-  //       meta: {
-  //         currentPage: {
-  //           name: 'sayhi',
-  //           title: 'Say Hi'
-  //         }
-  //       },
-  //       component: () => import('../views/Pages/SayHi.vue')
-  //     }
-  //   ]
-  // }
 ]
 
 const router = new VueRouter({
